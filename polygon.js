0c62c15ae649a64af058a10a565eee0aea7d9953
nanwabA <75519223+nanwabA@users.noreply.github.com>
2021-01-25 23:34:17 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,32 @@
+class Polygon { 
+
+constructor (){
+
+   var option={
+    'restitution':0.8,
+    'friction':0.3,
+    'density':1.0
+
+   }
+}
+
+this.body = Bodies.polygon(x, y,width,height, options);
+this.width = width;
+this.height = height;
+
+
+}
+
+display({
+polygon = bodies.circle(400,350,25)
+World.add(world,polygon);
+
+
+SlingShot= new SlingShot(this.polygon,{x:100,y:200})
+
+imageMode(CENTER)
+image(polygon_img,polygon.position.x,polygon.position.y,40,40);
+
+}
+
+
